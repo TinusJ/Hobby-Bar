73eaa104baa2396c7f133ac8209d0f7a1170e612
TinusJ <jacksontinus96@gmail.com>
2016-05-16 07:45:49 +0200

    initial ionic commit

--- README.md
+++
@@ -1,5 +0,0 @@
-# Hobby-Bar
-
-# TO DO
--1 : Change Name (Find Name)
--2 : 
--- typings/main.d.ts
+++ typings/main.d.ts
@@ -1,2 +1,1 @@
 /// <reference path="main/ambient/es6-shim/index.d.ts" />
-/// <reference path="main/ambient/jasmine/index.d.ts" />
